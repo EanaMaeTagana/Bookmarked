ef701a0ed82d2c456f815bc4fa08030c55fdefc9
EanaMaeTagana <144775623+EanaMaeTagana@users.noreply.github.com>
2026-01-16 04:42:02 +0400

    bug: CORS Issue

--- bookmarked/server/index.js
+++ bookmarked/server/index.js
@@ -23,8 +23,7 @@ mongoose.connect(process.env.MONGO_URI)
 // Middleware Configuration 
 app.use(cors({
   origin: [
-    'http://localhost:5173', 
-    'https://bookmarked-frontend.vercel.app'
+    'https://bookmarked-frontend.vercel.app/'
   ], 
   credentials: true,  
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
